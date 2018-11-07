b20094634c0181134831d7cbdbc34422b334005c
sang4 <sang4@illinois.edu>
2018-11-07 15:43:23 -0500

    finished

--- EdgeStreamPartners.py
+++ EdgeStreamPartners.py
@@ -1,58 +1,69 @@
 
+def getBestHand(hands):
+    """
+    Return the best hand name and corresponding deck
+    """
+    best = max(hands, key=rankHand)
+    return getName(best), best
 
-def poker(hands):
-    best = max(hands, key=hand_rank_orig)
-    return hand_rank(best), best
 
-
-def hand_rank(hand):
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):            # straight flush
+def getName(hand):
+    """
+    Return name of the hand
+    """
+    ranks = rankCard(hand)
+    if straight(ranks) and flush(hand):
         return "straight-flush"
-    elif kind(4, ranks):                           # 4 of a kind
+    elif kind(4, ranks):
         return "four-of-a-kind"
-    elif kind(3, ranks) and kind(2, ranks):        # full house
+    elif kind(3, ranks) and kind(2, ranks):
         return "full-house"
-    elif flush(hand):                              # flush
+    elif flush(hand):
         return "flush"
-    elif straight(ranks):                          # straight
+    elif straight(ranks):
         return "straight"
-    elif kind(3, ranks):                           # 3 of a kind
+    elif kind(3, ranks):
         return "three-of-a-kind"
-    elif two_pair(ranks):                          # 2 pair
+    elif twoPair(ranks):
         return "two-pairs"
-    elif kind(2, ranks):                           # kind
+    elif kind(2, ranks):
         return "one-pair"
-    else:                                          # high card
+    else:
         return "highest-card"
 
 
-def hand_rank_orig(hand):
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):            # straight flush
-        return (8, max(ranks))
-    elif kind(4, ranks):                           # 4 of a kind
-        return (7, kind(4, ranks), kind(1, ranks))
-    elif kind(3, ranks) and kind(2, ranks):        # full house
-        return (6, kind(3, ranks), kind(2, ranks))
-    elif flush(hand):                              # flush
-        return (5, ranks)
-    elif straight(ranks):                          # straight
-        return (4, max(ranks))
-    elif kind(3, ranks):                           # 3 of a kind
-        return (3, kind(3, ranks), ranks)
-    elif two_pair(ranks):                          # 2 pair
-        return (2, two_pair(ranks), ranks)
-    elif kind(2, ranks):                           # kind
-        return (1, kind(2, ranks), ranks)
-    else:                                          # high card
-        return (0, ranks)
+def rankHand(hand):
+    """
+    Return a number comparing the hand
+    Stronger the hand, higher the number
+    """
+    ranks = rankCard(hand)
+    if straight(ranks) and flush(hand):
+        return 8
+    elif kind(4, ranks):
+        return 7
+    elif kind(3, ranks) and kind(2, ranks):
+        return 6
+    elif flush(hand):
+        return 5
+    elif straight(ranks):
+        return 4
+    elif kind(3, ranks):
+        return 3
+    elif twoPair(ranks):
+        return 2
+    elif kind(2, ranks):
+        return 1
+    else:
+        return 0
 
 
 def straight(ranks):
-    "Return True if the ordered ranks form a 5-card straight."
-    min_num = ranks[0]
-    List = [min_num - i for i in range(len(ranks))]
+    """
+    Return true if it's a straight
+    """
+    minNum = ranks[0]
+    List = [minNum - i for i in range(len(ranks))]
     if List == ranks:
         return True
     else:
@@ -60,7 +71,9 @@ def straight(ranks):
 
 
 def flush(hand):
-    "Return True if all the cards have the same suit."
+    """
+    Return True if it's a flush
+    """
     suit = [s for n, s in hand]
     if [suit[0]]*len(suit) == suit:
         return True
@@ -69,7 +82,10 @@ def flush(hand):
 
 
 def kind(n, ranks):
-    "Return the first rank that this hand has exactly n of.Return None if there is no n-of-a-kind in the hand."
+    """
+    Return the rank if has exactly n of
+    Return None if there isn't any
+    """
     for r in ranks:
         if ranks.count(r) == n:
             return r
@@ -77,9 +93,11 @@ def kind(n, ranks):
     return None
 
 
-def two_pair(ranks):
-    """If there are two pair, return the two ranks as a
-    tuple: (highest, lowest); otherwise return None."""
+def twoPair(ranks):
+    """
+    Return two ranks as a tuple if there are two pairs
+    Return None otherwise
+    """
     pair = []
     for i in set(ranks):
         if ranks.count(i) == 2:
@@ -90,47 +108,44 @@ def two_pair(ranks):
         return None
 
 
-def card_ranks(hand):
-    "return a list of the ranks , sorted with higher first"
-    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
+def rankCard(hand):
+    """
+    Return a sorted list of the ranks 
+    """
+    
+    for r, s in hand:
+        ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
     ranks.sort(reverse=True)
     return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
 
+# This one doesn't work because I realized that Ace can also act as 1
+# def rankCard1(cards):
+#     """
+#     Return a list of the ranks
+#     """
+#     def map(r):
+#         if r == 'T':
+#             return 10
+#         elif r == 'J':
+#             return 11
+#         elif r == 'Q':
+#             return 12
+#         elif r == 'K':
+#             return 13
+#         elif r == 'A':
+#             return 14
+#         else:
+#             return int(r)
+#     ranks = [map(r) for r, s in cards]
+#     ranks.sort(reverse=True)
 
-def card_ranks1(cards):
-    "Return a list of the ranks, sorted with higher first."
-    def map(r):
-        if r == 'T':
-            return 10
-        elif r == 'J':
-            return 11
-        elif r == 'Q':
-            return 12
-        elif r == 'K':
-            return 13
-        elif r == 'A':
-            return 14
-        else:
-            return int(r)
-    ranks = [map(r) for r, s in cards]
-    ranks.sort(reverse=True)
-
-    return ranks
-
-# def allmax(iterable ,key = None):
-#     "return a list of all items equal to the max of the iterable"
-#     result , maxval =[],None
-#     key = key or (lambda: x:x)
-#     for x in iterable:
-#         xval = key(x)
-#         if not result or xval > maxval:
-#             result , maxval = [x] , xval
-#         elif xval == maxval:
-#             result.append(x)
-#     return result
+#     return ranks
 
 
-def printCombination(arr, n, r):
+def getCombinations(arr, n, r):
+    """
+    Return all combination of the given cards
+    """"
     data = [0]*r
     output = []
     combinationUtil(arr, data, 0,
@@ -154,13 +169,6 @@ def combinationUtil(arr, data, start,
         i += 1
 
 
-a = [
-    "2H 2S 3H 3S 3C",
-    "TH JH QC QD QS",
-    "KS AH 2H 3C 4H",
-    "3D 4S 5S 6D 7H",
-    "AD AS AH JD JS"
-]
 s = ["TH JH QC QD QS QH KH AH 2S 6S",
      "2H 2S 3H 3S 3C 2D 3D 6C 9C TH",
      "2H 2S 3H 3S 3C 2D 9C 3D 6C TH",
@@ -171,6 +179,7 @@ s = ["TH JH QC QD QS QH KH AH 2S 6S",
      "6C 9C 8C 2D 7C 2H TC 4C 9S AH",
      "3D 5S 2H QD TD 6S KH 9H AD QH"]
 
+# This supposedly correct output actually has some errors
 # Hand: TH JH QC QD QS Deck: QH KH AH 2S 6S Best hand: straight-flush
 # Hand: 2H 2S 3H 3S 3C Deck: 2D 3D 6C 9C TH Best hand: four-of-a-kind
 # Hand: 2H 2S 3H 3S 3C Deck: 2D 9C 3D 6C TH Best hand: full-house
@@ -182,15 +191,15 @@ s = ["TH JH QC QD QS QH KH AH 2S 6S",
 # Hand: 3D 5S 2H QD TD Deck: 6S KH 9H AD QH Best hand: highest-card
 
 
-def eval_deck(total):
+def evalDeck(total):
     r = 5
     n = len(total)
-    hands = printCombination(total, n, r)
-    return poker(hands)
+    hands = getCombinations(total, n, r)
+    return getBestHand(hands)
 
 
 for i in s:
     line = i.split(' ')
-    print("Hand: {hand} Deck: {deck} Best hand: {best}".format(
-        hand=i[:13], deck=i[13:], best=eval_deck(line)))
-    # print(eval_deck(line))
+    bestHand = evalDeck(line)
+    print("Hand: {hand} Deck: {deck} Best hand: {best} {card}".format(
+        hand=i[:14], deck=i[15:], best=bestHand[0], card= " ".join(bestHand[1])))
