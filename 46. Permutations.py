d4b1d63a71806c8bc8b4c0152a74512d9fbfc67c
sang4 <sang4@illinois.edu>
2018-11-07 01:46:31 -0500

    check

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -14,4 +14,36 @@ def permute(s):
     output = []
     permuteHelper(list(s), l, r, output)
     return output
-print(permute("CBASP"))
+
+# print(permute("CDBAMC"))
+
+
+
+def printCombination(arr, n, r): 
+    data = [0]*r
+    output = []
+    combinationUtil(arr, data, 0,  
+                    n - 1, 0, r, output)
+    return output
+def combinationUtil(arr, data, start,  
+                    end, index, r, output): 
+    t = []                      
+    if (index == r): 
+        for j in range(r): 
+            t.append(data[j])
+        output.append(t) 
+        return
+    i = start  
+    while(i <= end and end - i + 1 >= r - index): 
+        data[index] = arr[i]
+        combinationUtil(arr, data, i + 1,  
+                        end, index + 1, r, output)
+        i += 1
+s = "TH JH QC QD QS QH KH AH 2S 6S"
+arr = s.split(' ')
+r = 5; 
+n = len(arr); 
+combo = printCombination(arr, n, r)
+for i in combo:
+    print(i)
+print(len(combo))
