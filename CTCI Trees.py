af5a7c269991f3995f67fa2bbe383ad19ceb5865
sang4 <sang4@illinois.edu>
2018-11-23 23:23:58 -0500

    check

--- CTCI Trees.py
+++ CTCI Trees.py
@@ -108,18 +108,35 @@ def createMinimalBSTHelper(l, start, end):
 	n.right = createMinimalBSTHelper(l, mid + 1, end)
 	return n
 
-# Driver program to test above function 
-# root = Node(1) 
-# root.left = Node(2) 
-# root.right = Node(3) 
-# root.left.left = Node(4) 
-# root.left.right = Node(5) 
-# root.right.left = Node(6) 
-# root.right.right = Node(7) 
-# root.right.left.right = Node(8) 
+def printLevelOrder(root): 
+    if root is None: 
+        return
+      
+    queue = [] 
+    queue.append(root) 
+  
+    while(len(queue) > 0): 
+        print(queue[0].val)
+        node = queue.pop(0) 
+  
+        if node.left is not None: 
+            queue.append(node.left) 
+  
+        if node.right is not None: 
+            queue.append(node.right) 
+
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
+root.right.left = Node(6) 
+root.right.right = Node(7) 
+root.right.left.right = Node(8) 
   
 # print(rightView(root))
 
-arr = [1, 2, 3, 4, 5, 6, 7] 
-root = createMinimalBST(arr)
-printPreorder(root)
+# arr = [1, 2, 3, 4, 5, 6, 7] 
+# root = createMinimalBST(arr)
+# printPreorder(root)
+printLevelOrder(root)
